10e0f4e0ad6c8ea232f85e5f6c0609b13371782f
AdithyakrishnaKA <k.a.adithyakrishna7@gmail.com>
2026-07-15 20:26:31 +0530

    created a age verifier for election

---
+++ election_verifier.py
@@ -0,0 +1,10 @@
+age=int(input("enter your age:"))
+if age==0:
+    printf("your age is invalid")
+else:
+    print("your age is valid")
+if age>=18:
+    print("you are eligible to vote")
+else:
+    print("you are not eligible to vote")
+    
